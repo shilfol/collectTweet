23a0cfb61e6f14cb6e0949f57fc0a1c4354ce990
shilfol <yayananoda@gmail.com>
2018-05-15 03:41:53 +0900

    fetch data in detail

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func readKeys() {
 }
 
 func textRegExp(rawstr string) (name, plat string) {
-	regPlat := regexp.MustCompile(`[\s\S]*【(アイドルマスター シンデレラガールズ[　スターライトステージ]*)】[\s\S]+`)
+	regPlat := regexp.MustCompile(`[\s\S]*【(アイドルマスター シンデレラガールズ([\s　]*スターライトステージ)*)】[\s\S]+`)
 	regName := regexp.MustCompile(`[\s\S]+[\r\n|\n|\r\s](\S+)に投票したよ!![\s\S]+`)
 
 	platSlice := regPlat.FindStringSubmatch(rawstr)
@@ -63,11 +63,12 @@ func textRegExp(rawstr string) (name, plat string) {
 	return nameSlice[1], platSlice[1]
 }
 
-func processData(jsonPointer interface{}) (int, [][]string) {
+func processData(jsonPointer interface{}) (int, string, [][]string) {
 	outdatas := [][]string{}
 	datas := jsonPointer.(map[string]interface{})["modules"].([]interface{})
 
 	lastid := -1
+	lastTime := ""
 	for _, v := range datas {
 		out := v.(map[string]interface{})["status"]
 		data := out.(map[string]interface{})["data"]
@@ -77,14 +78,15 @@ func processData(jsonPointer interface{}) (int, [][]string) {
 		createTimeRaw, _ := time.Parse("Mon Jan 2 15:04:05 -0700 2006", text["created_at"].(string))
 		createTime := createTimeRaw.Add(9*time.Hour).Format("2006-01-02_15:04:05") + "_JST"
 		plat, name := textRegExp(text["text"].(string))
+		lastid, _ = strconv.Atoi(text["id_str"].(string))
+		lastTime = createTime
 		if plat != "" && name != "" {
 			fmt.Println(createTime, plat, name, user["screen_name"], text["id_str"])
-			lastid, _ = strconv.Atoi(text["id_str"].(string))
 			outdata := []string{createTime, plat, name, user["screen_name"].(string), text["id_str"].(string)}
 			outdatas = append(outdatas, outdata)
 		}
 	}
-	return lastid, outdatas
+	return lastid, lastTime, outdatas
 }
 
 func main() {
@@ -101,14 +103,29 @@ func main() {
 	outslice := strings.Split(string(out), ",")
 	lastid, _ := strconv.Atoi(strings.TrimRight(outslice[len(outslice)-1], "\n"))
 	queryID := lastid - 1
+	tweetTime, _ := time.Parse("2006-01-02_15:04:05_MST", outslice[0])
+	sinceTime := tweetTime.Add(-1*time.Hour).Format("2006-01-02_15:04:05") + "_JST"
+	untilTime := tweetTime.Format("2006-01-02_15:04:05") + "_JST"
 
 	endPointRaw := "https://api.twitter.com/1.1/search/universal.json"
 
 	outdatas := [][]string{}
 
-	for i := 0; i < 170; i++ {
-		queryRaw := "【アイドルマスター シンデレラガールズ】 -RT max_id:" + fmt.Sprint(queryID)
-		params := map[string]string{"q": queryRaw, "modules": "status", "count": "100"}
+	writefile, err := os.OpenFile(os.Args[2], os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer writefile.Close()
+
+	writer := csv.NewWriter(writefile)
+
+	for i := 0; i < 800; i++ {
+		queryRaw := "【アイドルマスター シンデレラガールズ】 -RT"
+		if queryID > 0 {
+			queryRaw += " max_id:" + fmt.Sprint(queryID)
+		}
+		queryRaw += " since:" + sinceTime + " until:" + untilTime
+		params := map[string]string{"q": queryRaw, "modules": "status", "count": "100", "result_type": "recent"}
 
 		response, err := consumer.Get(endPointRaw, params, accessToken)
 		if err != nil {
@@ -132,11 +149,14 @@ func main() {
 			log.Fatal(err)
 		}
 
-		if retid, retdatas := processData(jsonPointer); retid < 0 {
+		if retid, retTime, retdatas := processData(jsonPointer); retid < 0 {
 			fmt.Println("no id")
 			break
 		} else {
 			queryID = retid - 1
+			tweetTime, _ = time.Parse("2006-01-02_15:04:05_MST", retTime)
+			sinceTime = tweetTime.Add(-1*time.Hour).Format("2006-01-02_15:04:05") + "_JST"
+			untilTime = tweetTime.Format("2006-01-02_15:04:05") + "_JST"
 			outdatas = append(outdatas, retdatas...)
 		}
 
@@ -144,19 +164,24 @@ func main() {
 
 		if remCount, _ := strconv.Atoi(response.Header["X-Rate-Limit-Remaining"][0]); remCount <= 0 {
 			fmt.Println("no remain count")
+			limittimenum, _ := strconv.ParseInt(response.Header["X-Rate-Limit-Reset"][0], 10, 64)
+			fmt.Println("next reset time: ", time.Unix(limittimenum, 0))
 			break
 		} else {
+			limittimenum, _ := strconv.ParseInt(response.Header["X-Rate-Limit-Reset"][0], 10, 64)
+			fmt.Print("next reset time: ", time.Unix(limittimenum, 0))
+
 			fmt.Println("remain: ", remCount)
 		}
-	}
 
-	writefile, err := os.OpenFile(os.Args[2], os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		panic(err)
+		if i%100 == 0 {
+			if err := writer.WriteAll(outdatas); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("write!")
+			outdatas = [][]string{}
+		}
 	}
-	defer writefile.Close()
-
-	writer := csv.NewWriter(writefile)
 	if err := writer.WriteAll(outdatas); err != nil {
 		log.Fatal(err)
 	}
